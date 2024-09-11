e902b42fca19387aa094ab657b518f1d38cfcdf7
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-09-11 23:25:46 +0530

    added shadcn

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Component from './components/issue.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <Component />
+    <App />
   </StrictMode>,
 )
