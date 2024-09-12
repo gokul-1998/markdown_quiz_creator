1e022a59f6fdfa25149072f5e3c5582f9651ef31
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-09-12 23:03:01 +0530

    removed other issue creator

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Component from './components/issues.jsx'
 import IssueCreator from './components/issue2.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
